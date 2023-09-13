6a828a5817ff42d6a4672adef18b33022faaa98e
lovegupta112 <loveguptaofficials@gmail.com>
2023-09-13 17:04:58 +0530

    added express for server

--- index.js
+++ index.js
@@ -1,75 +1,77 @@
-const fs = require('fs').promises;
-const http = require('http');
+const express=require('express');
+const app=express();
+const path=require('path');
 const uuid = require('uuid');
 
-const port = 8000;
-const host = 'localhost';
 
-let indexFile;
-let jsonData;
+//for default route ---------
+app.get('/',(req,res)=>{
+  res.send('<h1>Welcome to Our Page !</h1>');
+})
 
-const requestListener = function (req, res) {
-  const url = req.url;
-
-  //for GET /html -------
-  if (req.method === 'GET' && url === '/html') {
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200);
-    res.end(indexFile);
-  } else if (req.method === 'GET' && url === '/json') {
-    //for GET /json -------
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200);
-    res.end(jsonData);
-  } else if (req.method === 'GET' && url === '/uuid') {
-    //for GET /uuid -------
-    const generateUuid = { uuid: uuid.v4() };
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200);
-    res.end(JSON.stringify(generateUuid));
-  } else if (req.method === 'GET' && url.split('/').length === 3) {
-    const urlPartsArr = req.url.split('/');
-    const num = parseInt(urlPartsArr[2]);
-
-    if (urlPartsArr[1] === 'status' && !isNaN(num)) {
-      //for GET /status/{status_code}-------
-      res.setHeader('Content-Type', 'text/plain');
-      res.writeHead(num);
-      res.end(`Response with status code ${num}`);
-      return;
-    } else if (urlPartsArr[1] === 'delay' && !isNaN(num)) {
-      //forGET /delay/{delay_in_seconds}  -------
-      res.setHeader('Content-Type', 'text/plain');
-      res.writeHead(200);
-      setTimeout(() => {
-        res.end(`After ${num} sec \n Response with status code 200 !`);
-      }, num * 1000);
+//for html route---------
+app.get('/html',(req,res)=>{
+    try{
+    res.sendFile(path.join(__dirname,'index.html'));
     }
-  } else {
-    res.setHeader('Content-Type', 'text/plain');
-    res.writeHead(404);
-    res.end('Not found !');
+    catch(error){
+      console.log('Error: ',error);
+      res.status(404).send(`Sorry, cant find that `);
+    }
+})
+
+//for json route --------------
+app.get('/json',(req,res)=>{
+  try{
+    res.sendFile(path.join(__dirname,'sample.json'));
   }
-};
-
-const server = http.createServer(requestListener);
-
-async function startServer() {
-  try {
-    //we will load the Html content and json from index.html and sample.json respectively
-    //and save the html content , json  to our global  variable indexFile and  jsonData  respectively
-
-    indexFile = await fs.readFile(__dirname + '/index.html', 'utf-8');
-    jsonData = await fs.readFile(__dirname + '/sample.json', 'utf-8');
-
-    //we will start our server -----
-    server.listen(port, host, () => {
-      console.log(`Server is running on http://${host}:${port}`);
-    });
-  } catch (error) {
-    console.error('Error: ', error);
-    process.exit(1); //terminating process synchronously -------
+  catch(error){
+     console.log('Error: ',error);
+     res.status(404).send(`Sorry, cant find that `);
   }
+})
+
+//for uuid route--------------------
+app.get('/uuid',(req,res)=>{
+   try{
+    res.send({uuid:uuid.v4()});
+   }
+   catch(error){
+    console.log('Error: ',error);
+    res.status(404).send(`Sorry, cant find that `);
+   }
+})
+
+//for status_code route ------------------
+app.get('/status/:status_code',(req,res)=>{
+  try{
+       const statusCode=req.params.status_code;
+       res.status(statusCode).send(`status code : ${statusCode}`);
+   }
+   catch(error){
+    console.log('Error: ',error);
+    res.status(404).send(`Sorry, cant find that `);
+   }
+})
+
+//for delay route -------------
+
+app.get('/delay/:delay_in_seconds',(req,res)=>{
+  try{
+    const delay=req.params.delay_in_seconds;
+    setTimeout(() => {
+      res.send('Response with 200 status code !');
+    },delay*1000);
+    console.log(delay);
 }
+catch(error){
+ console.log('Error: ',error);
+ res.status(404).send(`Sorry, cant find that `);
+}
+})
 
-startServer();
+const PORT=process.env.port || 5000;
+
+app.listen(PORT,()=>{
+   console.log(`Server is running on port ${PORT}`);
+})
